4f9d0c6804e1cb919ddec7a57f3c784cc05c69f1
dkoh2018 <105094753+dkoh2018@users.noreply.github.com>
2025-07-06 03:10:09 -0500

    bugs

--- src/components/chat/MessageInputContainer.tsx
+++ src/components/chat/MessageInputContainer.tsx
@@ -7,7 +7,7 @@ interface MessageInputContainerProps {
 export const MessageInputContainer: React.FC<MessageInputContainerProps> = ({ children }) => {
   return (
     <div className="absolute bottom-12 left-0 right-0 z-50 bg-transparent px-3 sm:px-4 py-4">
-      <div className="w-full max-w-full mx-auto relative message-input-responsive">
+      <div className="w-full mx-auto relative max-w-full sm:max-w-[700px] lg:max-w-[896px] xl:max-w-[1024px]">
         {children}
       </div>
     </div>
